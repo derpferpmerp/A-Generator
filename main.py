864c1be0d65b5ca7a6918202d93739c073632904
Cole Fleming <47375701+derpferpmerp@users.noreply.github.com>
2021-07-21 19:37:28 +0000

    Added Grid Creation (Randomized, No Grouping) and also added the ability to grab the data value of nearby squares relative to a coordinate

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-def distance(x1,y1,x2,y2,method=False):
-	if not method: return None
-	elif method == "diag":
-		dx,dy = [abs(x1 - x2),abs(y1 - y2)]
-	return abs(x1-x2) + abs(y1-y2)
+import numpy as np
+import random
 
-print(ab_distance(0,3,4,0))
+
+def gen_rand_grid(d, density=1):
+	try:
+		grid = np.zeros((d, d), int)
+		choices = np.random.choice(grid.size,random.randrange(round((3 / 4) * d * density), d * density), replace=False)
+		grid.ravel()[choices] = 1
+		return grid
+	except ValueError:
+		return None
+
+def get_bounds(x1,y1,arr):
+	dctout = {}
+	for direction,cl in [["l",[-1,0]],["r",[1,0]],["u",[0,-1]],["d",[0,1]]]:
+		for coord in [x1 + cl[0],y1 + cl[1]]:
+			#print(f"{direction}: {coord}")
+			if coord < 0:
+				cont = False
+				break
+			cont = True
+
+		if not cont: continue
+		dctout[direction] = arr[y1 + cl[0],x1 + cl[1]]
+		#print([y1 + cl[0],x1 + cl[1]])
+	return dctout
+
+grd = gen_rand_grid(6, density=2)
+print(grd)
+print(get_bounds(0,0,grd))
